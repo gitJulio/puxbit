8bd84812515c58977e44dd9254e2ed46934c63dd
Julio Bueso <julio.ezequiel@gmail.com>
2018-11-20 23:10:21 -0600

    segundo

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const http =require('http');
 http.createServer((req, res)=>{
   res.write('Hola Mundo');
   res.end();
-}).listen(8080);
+}).listen(3000);
 
-console.log('Escuchando el puerto 8080');
+console.log('Escuchando el puerto 80');
