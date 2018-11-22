b2520baf2963da79079a0e9a3404140b7bcdfa5f
Julio Bueso <julio.ezequiel@gmail.com>
2018-11-21 20:35:59 -0600

    #

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  // res.send('Hello World')
+  let salida={
+    nombre:"Julio",
+    apellido:"Bueso",
+    edad:30,
+    url:"/"
+  }
+  res.send(salida)
 })
 
-app.listen(3000)
+app.get('/data', function (req,res){
+  res.send("Hola Data")
+});
+
+app.listen(3000, ()=>{
+  console.log("Escuchando peticiones en el puerto 3000");
+})
